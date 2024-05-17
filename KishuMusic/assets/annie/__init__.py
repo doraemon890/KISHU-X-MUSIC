41061fd73e36ab12598789a8a583d48d8fdc11a0
RAJNISH XD <155803358+doraemon890@users.noreply.github.com>
2024-05-17 12:52:58 +0530

    Update __init__.py

--- KishuMusic/assets/annie/__init__.py
+++ KishuMusic/assets/annie/__init__.py
@@ -3,6 +3,6 @@ import os
 thumbs = []
 colors = ["white", "black", "red", "orange", "yellow", "green", "cyan", "azure", "blue", "violet", "magenta", "pink"]
 
-for filename in os.listdir("./Kishumusic/assets/annie"):
+for filename in os.listdir("./KishuMusic/assets/annie"):
     if filename.endswith("png"):
         thumbs.append(filename[:-4])
