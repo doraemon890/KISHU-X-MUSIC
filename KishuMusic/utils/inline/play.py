88f395b21f955457dc98b39cdaf57b1db6733c1e
RAJNISH XD <155803358+doraemon890@users.noreply.github.com>
2024-05-17 14:04:12 +0530

    Update play.py

--- KishuMusic/utils/inline/play.py
+++ KishuMusic/utils/inline/play.py
@@ -1,7 +1,7 @@
 import math
-
 from pyrogram.types import InlineKeyboardButton
-
+from KishuMusic import app
+import config
 from KishuMusic.utils.formatters import time_to_seconds
 
 
@@ -33,42 +33,39 @@ def stream_markup_timer(_, chat_id, played, dur):
     percentage = (played_sec / duration_sec) * 100
     umm = math.floor(percentage)
     if 0 < umm <= 10:
-        bar = "✧━━━━━━━━━━"
+        bar = "◉—————————"
     elif 10 < umm < 20:
-        bar = "━━✧━━━━━━━━"
+        bar = "—◉————————"
     elif 20 <= umm < 30:
-        bar = "━━━━✧━━━━━━"
+        bar = "——◉———————"
     elif 30 <= umm < 40:
-        bar = "━━━━━✧━━━━━"
+        bar = "———◉——————"
     elif 40 <= umm < 50:
-        bar = "━━━━━━✧━━━━"
+        bar = "————◉—————"
     elif 50 <= umm < 60:
-        bar = "━━━━━━━✧━━━"
+        bar = "—————◉————"
     elif 60 <= umm < 70:
-        bar = "━━━━━━━━✧━━"
+        bar = "——————◉———"
     elif 70 <= umm < 80:
-        bar = "━━━━━━━━✧━━"
+        bar = "———————◉——"
     elif 80 <= umm < 95:
-        bar = "━━━━━━━━━✧━"
+        bar = "————————◉—"
     else:
-        bar = "━━━━━━━━━━✧"
-
-
+        bar = "—————————◉"
     buttons = [
-        
-        [
-            InlineKeyboardButton(text=f"{played} {bar} {dur}", callback_data="GetTimer",)
+                [
+            InlineKeyboardButton(
+                text=f"{played} {bar} {dur}",
+                callback_data="GetTimer",
+            )
         ],
         [
             InlineKeyboardButton(text="▷", callback_data=f"ADMIN Resume|{chat_id}"),
             InlineKeyboardButton(text="II", callback_data=f"ADMIN Pause|{chat_id}"),
-            InlineKeyboardButton(text="↻", callback_data=f"ADMIN Replay|{chat_id}"),
-        ],
-        [
             InlineKeyboardButton(text="‣‣I", callback_data=f"ADMIN Skip|{chat_id}"),
             InlineKeyboardButton(text="▢", callback_data=f"ADMIN Stop|{chat_id}"),
         ],
-       
+        
         [InlineKeyboardButton(text=_["CLOSE_BUTTON"], callback_data="close")],
     ]
     return buttons
@@ -79,10 +76,10 @@ def stream_markup(_, chat_id):
         [
             InlineKeyboardButton(text="▷", callback_data=f"ADMIN Resume|{chat_id}"),
             InlineKeyboardButton(text="II", callback_data=f"ADMIN Pause|{chat_id}"),
-            InlineKeyboardButton(text="↻", callback_data=f"ADMIN Replay|{chat_id}"),
             InlineKeyboardButton(text="‣‣I", callback_data=f"ADMIN Skip|{chat_id}"),
             InlineKeyboardButton(text="▢", callback_data=f"ADMIN Stop|{chat_id}"),
         ],
+            
         [InlineKeyboardButton(text=_["CLOSE_BUTTON"], callback_data="close")],
     ]
     return buttons
