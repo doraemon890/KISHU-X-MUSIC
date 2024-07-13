fb06f1e0a0e8e5c53f73f71820ea44006b303de0
RAJNISH XD <155803358+doraemon890@users.noreply.github.com>
2024-07-13 10:48:24 +0530

    Update download.py

--- KishuMusic/plugins/tools/download.py
+++ KishuMusic/plugins/tools/download.py
@@ -71,33 +71,6 @@ def download_song(_, message):
     except Exception as e:
         print(e)
         
-# ------------------------------------------------------------------------------- #
-
-###### INSTAGRAM REELS DOWNLOAD
-
-@app.on_message(filters.command("insta"))
-async def download_instagram_reel(client, message):
-    try:
-        if len(message.text.split(" ")) == 1:
-            await message.reply_text("Please provide an Instagram link after the command.")
-            return
-        
-        url = message.text.split(" ", 1)[1]
-        response = requests.post(f"https://api.qewertyy.dev/download/instagram?url={url}")
-        
-        if response.status_code == 200:
-            data = response.json()
-            if "content" in data and len(data["content"]) > 0:
-                video_url = data["content"][0]["url"]
-                await message.reply_video(video_url)
-            else:
-                await message.reply_text("No content found in the response.")
-        else:
-            await message.reply_text(f"Request failed with status code: {response.status_code}")
-    except Exception as e:
-        await message.reply_text(f"Something went wrong: {e}")
-
-# --------------
 
 def get_file_extension_from_url(url):
     url_path = urlparse(url).path
